bd4b90078a5510b283f8d6c53e8be3869a2d1307
wgeorge1990 <williameverettgeorge@icloud.com>
2019-01-29 01:22:49 -0500

    Done.

--- index.js
+++ index.js
@@ -8,8 +8,15 @@ You should NOT try to redefine any of the above variables.
 
 // Write your code below this line!
 
+//newID Adds oldID which too one billion
 let newID = 1000000000 + oldID
-var ageIsValid = Number.isInteger(currentAge)
-let randomNumber = Math.floor(Math.random() * 20) + 1
-let randomInteger = Math.floor(randomNumber)
-let randomUserID = 1000000000 + (Math.floor(Math.random() * 20) + 1)
+
+//checks integer value of currentAge(integer) and returns true or false.
+const ageIsValid = Number.isInteger(currentAge)
+
+//Generates random number between 0 and 20
+const randomNumber = (Math.random() * 10) + 1
+
+const randomInteger = Math.floor(randomNumber)
+
+let randomUserID = randomInteger + 1000000000
